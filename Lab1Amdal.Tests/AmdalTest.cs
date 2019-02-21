7d30a535b814db4f3445ed5750b99d729e653d60
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-02-21 09:35:41 +0300

    Added Amdal

---
+++ Lab1Amdal.Tests/AmdalTest.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+using Lab1Amdal;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [TestCaseSource("Cases")]
+        public void AmdalRTest(Amdal amdal, double expected)
+        {
+            double actual = amdal.R;
+            Assert.AreEqual(expected, actual);
+        }
+
+        static object[] Cases =
+        {
+            new object[] { new Amdal(0.7, 0, 1), 1 },
+            new object[] { new Amdal(1, 0, 34), 1 },
+            new object[] { new Amdal(1, 1, 88), 0.5 },
+        };
+    }
+}
---
+++ Lab1Amdal/Amdal.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1Amdal
+{
+    public class Amdal
+    {
+        private double a;
+        private double c;
+        private int n;
+
+        public Amdal() { }
+
+        public Amdal(double a, double c, int n)
+        {
+            A = a;
+            C = c;
+            N = n;
+        }
+
+        public double R
+        {
+            get
+            {
+                return 1 / (a + ((1 - a) / n) + c);
+            }
+        }
+
+        public double A
+        {
+            get
+            {
+                return a;
+            }
+
+            set
+            {
+                if (value >= 0 && value <= 1)
+                {
+                    a = value;
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+            }
+        }
+
+        public double C
+        {
+            get
+            {
+                return c;
+            }
+
+            set
+            {
+                if (value >= 0)
+                {
+                    c = value;
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+            }
+        }
+
+        public int N
+        {
+            get
+            {
+                return n;
+            }
+
+            set
+            {
+                if (value > 0)
+                {
+                    n = value;
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+            }
+        }
+    }
+}
